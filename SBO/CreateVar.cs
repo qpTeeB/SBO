79aa738c1b45a507e6587b0c72febfbafaf5306f
Vadim <kvuuhh@gmail.com>
2022-12-19 20:24:15 +0400

    Создание сигнатуры формы выбора товара

---
+++ SBO/CreateVar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SBO
+{
+    public partial class CreateVar : Form
+    {
+        public CreateVar()
+        {
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+        }
+        Method1()
+        {
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+        }
+        Method2()
+        {
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaaa
+        }
+    }
+}
